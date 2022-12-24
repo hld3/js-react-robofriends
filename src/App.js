b6d61661ec3886259f6e79b7ddc73320e45918e5
Harry Dodson <harry.lee.dodson@gmail.com>
2022-12-24 20:49:44 +0000

    componentDidMount and fetch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
+/**
+ * Just a note. App is a smart component. // ! Has State.
+ * SearchBox and CardList are pure functions. Given the same input they will always have the same output.
+ */
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         };
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
+    }
+
 
     onSearchChange = (event) => {
         this.setState({ searchField: event.target.value })
@@ -20,6 +30,7 @@ class App extends Component {
     render() {
         const filteredBots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+                || robots.email.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
         return (
             <div className="tc">
